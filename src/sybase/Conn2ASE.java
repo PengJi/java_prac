f5cdd91647cc8a95c23bc5fd854b18f5106b96b3
pengji <jipeng92@gmail.com>
2016-04-16 10:53:16 +0800

    modified:   src/sybase/Conn2ASE.java

--- src/sybase/Conn2ASE.java
+++ src/sybase/Conn2ASE.java
@@ -2,15 +2,105 @@ package sybase;
 
 import java.sql.*; 
 import java.util.*;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.BufferedInputStream;
 
 public class Conn2ASE {
+	public static String dbUser = null;
+	public static String dbPasswd = null;
+	public static String dbName = null;
+	public static String dbTableName = null;
+	public static String fs = null;
+	
+	//写文件
+	public static void write(String filepath,String content){
+		FileOutputStream outputStream = null;
+		BufferedOutputStream bufferedOutputStream = null;
+		File file = new File(filepath);
+		
+		try {
+			outputStream = new FileOutputStream(file);
+			bufferedOutputStream = new BufferedOutputStream(outputStream);
+			bufferedOutputStream.write(content.getBytes());			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally{
+			try {
+				outputStream.close();
+				bufferedOutputStream.flush();
+				bufferedOutputStream.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	//读文件
+	public static void read(String filepath){
+		BufferedInputStream bufferedInputStream = null;
+		StringBuffer stringBuffer = new StringBuffer();
+		File file = new File(filepath);
+		
+		try {
+			int len = 512;
+			byte[] bytes = new byte[len];
+			bufferedInputStream = new BufferedInputStream(new FileInputStream(file));
+			while((len = bufferedInputStream.read(bytes,0,len)) != -1){
+				if(len < 512){
+					byte[] tmpBuf = new byte[len];
+					System.arraycopy(bytes, 0, tmpBuf, 0, len);
+					stringBuffer.append(new String(tmpBuf));
+					tmpBuf = null;
+				}else{
+					stringBuffer.append(new String(bytes));
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				bufferedInputStream.close();
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+	}
+	
+	//得到表
+	public static String getTable(String user,String passwd,String dbString,String tableName){
+		String tables = null;
+		try {
+			Class.forName("com.sybase.jdbc4.jdbc.SybDriver").newInstance();
+			String url = "jdbc:sybase:Tds:192.168.101.62:5000/" + dbString;
+			Properties sysProps = System.getProperties();
+            sysProps.put("user", user); 
+            sysProps.put("password", passwd);
+            Connection conn = DriverManager.getConnection(url, sysProps);
+            Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_UPDATABLE);
+            String sql = "select * from " + tableName; //查询表
+            ResultSet rs = stmt.executeQuery(sql);
+            while (rs.next()) {
+				tables = tables + "oject_id:"+rs.getString(1)+",oject_name:"+rs.getString(2);
+            }
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return tables;
+	}
+	
     public static void main(String[] args) {
+    	/*
         try {
-            Class.forName("com.sybase.jdbc3.jdbc.SybDriver").newInstance();
-            String url = "jdbc:sybase:Tds:192.168.100.120:5000/master";// 数据库名
+            Class.forName("com.sybase.jdbc4.jdbc.SybDriver").newInstance();
+            String url = "jdbc:sybase:Tds:192.168.101.62:5000/master";// 数据库名
             Properties sysProps = System.getProperties();
             sysProps.put("user", "sa"); // 设置数据库访问用户名
-            sysProps.put("password", "560128"); // 密码
+            sysProps.put("password", "jipeng1008"); // 密码
             Connection conn = DriverManager.getConnection(url, sysProps);
             Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_UPDATABLE);
             String sql = "select id,name,crdate from dbo.sysobjects where type='U'"; // 表
@@ -24,5 +114,15 @@ public class Conn2ASE {
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
+        */
+        
+        dbUser = "sa";
+        dbPasswd = "jipeng1008";
+        dbName = "sybsystemdb";
+        dbTableName = "sysobjects";
+        fs = "/home/sybase/test.txt";
+        String tableStr = getTable(dbUser, dbPasswd, dbName, dbTableName);
+        write(fs,tableStr);
+        read(fs);
     }
 }
